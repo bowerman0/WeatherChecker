3c7e4dce3a69ae9379b677713aa5fac214991fe5
Michael Bowerman <michael.bowerman@gmail.com>
2015-05-03 10:44:29 -0500

    Update weather contract to include location table data. Update packages and imports to remove sunshine references.

--- app/src/androidTest/java/com/example/udacityapp/data/TestPractice.java
+++ app/src/androidTest/java/com/example/udacityapp/data/TestPractice.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app.data;
+package com.example.udacityapp.data;
 
 import android.test.AndroidTestCase;
 
--- app/src/androidTest/java/com/example/udacityapp/data/TestUtilities.java
+++ app/src/androidTest/java/com/example/udacityapp/data/TestUtilities.java
@@ -1,4 +1,4 @@
-package com.example.android.sunshine.app.data;
+package com.example.udacityapp.data;
 
 import android.content.ContentValues;
 import android.database.ContentObserver;
@@ -8,7 +8,7 @@ import android.os.Handler;
 import android.os.HandlerThread;
 import android.test.AndroidTestCase;
 
-import com.example.android.sunshine.app.utils.PollingCheck;
+import com.example.udacityapp.utils.PollingCheck;
 
 import java.util.Map;
 import java.util.Set;
